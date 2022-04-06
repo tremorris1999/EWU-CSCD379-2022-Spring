b89430ba3d82f3bbddef370337a161b7876658af
Trevor Morris <tremorris1999@gmail.com>
2022-04-06 19:32:00 -0400

    reinit project

--- wordle-ad-farm/nuxt.config.js
+++ wordle-ad-farm/nuxt.config.js
@@ -41,7 +41,16 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
