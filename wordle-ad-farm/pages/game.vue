01e12d6f9d0b3747f668a38bda0d9bac3dd4702a
Trevor Morris <tremorris1999@gmail.com>
2022-04-11 22:59:07 -0700

    try to clean index.vue

--- wordle-ad-farm/pages/game.vue
+++ wordle-ad-farm/pages/game.vue
@@ -7,17 +7,15 @@
             
             <!-- <v-row > -->
             
-            <p>you're being exploited for ad revenue. please wait a moment...</p>
-<!--             
-        </v-row> -->
-    </v-card>
+            <p>you're being exploited for ad revenue. please wait a moment...</p>             
+       <!-- </v-row> -->
+        </v-card>
+
     <v-card v-if="isLoading">
         <NuxtLogo />
         <VuetifyLogo />
         <v-card-text>
-            <p style="h1"> 
-                thanks for playing our game!
-            </p>            
+            <h1>thanks for playing our game!</h1>
             <v-btn color="primary" nuxt to="/"> 
             <!-- index.vue is implicit, so only route to / -->
                 home page
