629c0b32f8207d21fc7cf07c1152ae2ac776fb09
Trevor Morris <tremorris1999@gmail.com>
2022-04-21 01:53:17 -0700

    fixed gitignore

---
+++ wordle-web/components/NotWordleLogo.vue
@@ -0,0 +1,18 @@
+<template>
+  <img class="notwordle-logo" alt="!Wordle Logo" src="/file-word-box.svg" />
+</template>
+
+<style>
+.notwordle-logo {
+  height: 180px;
+  width: 180px;
+  transform: rotateY(720deg);
+  animation: turn 2.5s ease-out forwards 0.5s;
+}
+
+@keyframes turn {
+  100% {
+    transform: rotateY(0deg);
+  }
+}
+</style>
---
+++ wordle-web/pages/game.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-container fluid fill-height justify-center>
+    <v-container v-if="!isLoaded" fluid fill-height justify-center>
+      <v-card loading>
+        <v-card-title class="justify-center">
+          You're being exploited for ad revenue, please standby...
+        </v-card-title>
+        <PrerollAd />
+      </v-card>
+    </v-container>
+
+    <v-container v-if="isLoaded" fluid fill-height justify-center>
+      <v-tooltip bottom>
+        <template #activator="{ on, attrs }">
+          <v-btn color="primary" nuxt to="/" fab v-bind="attrs" v-on="on">
+            <v-icon> mdi-home </v-icon>
+          </v-btn>
+        </template>
+        <span> Go Home </span>
+      </v-tooltip>
+
+      <v-card-text class="text-h1 font-weight-black text-center">
+        !Wordle
+      </v-card-text>
+
+      <v-alert v-if="wordleGame.gameOver" width="80%" :type="gameResult.type">
+        {{ gameResult.text }}
+        <v-btn class="ml-2" @click="resetGame"> Play Again? </v-btn>
+      </v-alert>
+
+      <game-board :wordleGame="wordleGame" />
+
+      <keyboard :wordleGame="wordleGame" />
+    </v-container>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { WordsService } from '~/scripts/wordsService'
+import { GameState, WordleGame } from '~/scripts/wordleGame'
+import KeyBoard from '@/components/keyboard.vue'
+import GameBoard from '@/components/game-board.vue'
+import { Word } from '~/scripts/word'
+
+@Component({ components: { KeyBoard, GameBoard } })
+export default class Game extends Vue {
+  word: string = WordsService.getRandomWord()
+  wordleGame = new WordleGame(this.word)
+
+  isLoaded: boolean = false
+
+  mounted() {
+    setTimeout(() => {
+      this.isLoaded = true
+    }, 5000)
+  }
+
+  resetGame() {
+    this.word = WordsService.getRandomWord()
+    this.wordleGame = new WordleGame(this.word)
+  }
+
+  get gameResult() {
+    if (this.wordleGame.state === GameState.Won) {
+      return { type: 'success', text: 'Yay! You won!' }
+    }
+    if (this.wordleGame.state === GameState.Lost) {
+      return { type: 'error', text: `You lost... :( The word was ${this.word}` }
+    }
+    return { type: '', text: '' }
+  }
+
+  getLetter(row: number, index: number) {
+    const word: Word = this.wordleGame.words[row - 1]
+    if (word !== undefined) {
+      return word.letters[index - 1]?.char ?? ''
+    }
+    return ''
+  }
+}
+</script>
