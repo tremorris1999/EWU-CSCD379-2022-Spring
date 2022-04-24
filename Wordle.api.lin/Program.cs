e87184c624cc5bc245898138a929c6f8acc8bffe
Trevor Morris <tremorris1999@gmail.com>
2022-04-24 12:59:04 -0700

    changed swagger ui settings

--- Wordle.api.lin/Program.cs
+++ Wordle.api.lin/Program.cs
@@ -21,6 +21,7 @@ var app = builder.Build();
     app.UseSwagger();
     app.UseSwaggerUI(options =>
     {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
         options.RoutePrefix = string.Empty;
     });
 //}
