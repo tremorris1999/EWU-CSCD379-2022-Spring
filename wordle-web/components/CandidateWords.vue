0be788bf4f9d4b7469d1b774dac161b504e160f6
Trevor Morris <tremorris1999@gmail.com>
2022-04-25 22:24:29 -0700

    started CandidateWords component

---
+++ wordle-web/components/CandidateWords.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container>
+        <v-card>
+            <v-card-title>
+                Test
+            </v-card-title>
+        </v-card>
+    </v-container>    
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+
+@Component
+export default class CandidateWords extends Vue
+{
+
+}
+</script>
--- wordle-web/pages/testValidWords.vue
+++ wordle-web/pages/testValidWords.vue
@@ -1,49 +1,16 @@
 <template>
   <v-container>
-    <v-card color="grey lighten-4" flat height="200px" tile>
-      <v-toolbar>
-        <v-text-field
-          id="searchField"
-          hide-details
-          prepend-icon="mdi-magnify"
-          single-line
-          @input="logMatches(value)"
-        >
+    <v-card color="grey lighten-4">
           <CandidateWords />
-        </v-text-field>
-        <v-btn icon>
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </v-toolbar>
     </v-card>
   </v-container>
-  <!--    
-    <v-slot></v-slot>
-    <v-virtual-scroll
-      height="100"
-      item-height="20"
-      items="getCandidateWords"
-    ></v-virtual-scroll> -->
 </template>
+
 <script lang="ts">
-import { Component, Vue } from 'vue-class-component'
-// import CandidateWords from '~/components/valid-words.vue'
+import { Component, Vue } from 'vue-property-decorator'
 
-import { WordsService } from '@/scripts/wordsService'
 @Component
-export default class CandidateWords extends Vue {
-  // remainingWords: string[] = [WordsService.#words]
-  //   guess : string
-  //   public getCandidateWords(guess:string){
-  //       this.guess = guess;
-  //   }
+export default class TestPage extends Vue {
 
-  static match: string[] = []
-
-  static getMatches(guess: string): string[] {
-    CandidateWords.match = WordsService.getCandWords(guess)
-    console.log(CandidateWords.match)
-    return CandidateWords.match
-  }
 }
 </script>
