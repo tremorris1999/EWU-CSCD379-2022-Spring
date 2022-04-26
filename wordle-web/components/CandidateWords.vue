ab86b158c2babd8d5fe249935088a6f3c39fd76a
schuyler <sasplin@ewu.edu>
2022-04-26 01:40:44 -0700

    this works but don't know why it works. committing for reference.

--- wordle-web/components/CandidateWords.vue
+++ wordle-web/components/CandidateWords.vue
@@ -1,43 +1,58 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col>
-                <v-list>
-                    <v-list-item v-for="c in candidates" :key="c">
-                        <v-list-item-content>
-                            {{c}}
-                        </v-list-item-content>
-                    </v-list-item>
-                </v-list>
-            </v-col>
-        </v-row>
-    </v-container>    
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-list>
+          <v-list-item v-for="c in candidates" :key="c">
+            <v-list-item-content>
+              {{ c }}
+            </v-list-item-content>
+          </v-list-item>
+          <v-btn icon>
+            <v-icon @click="$emit('log')">mdi-magnify</v-icon>
+          </v-btn>
+          <!-- "CandidateWords.update(this.value)" -->
+          <v-text-field @input="log(text)">
+            <input v-model="text"
+          /></v-text-field>
+
+          <!-- @input="" -->
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator'
-import {WordsService} from '~/scripts/wordsService'
+import { Component, Vue } from 'vue-property-decorator'
+import { WordsService } from '~/scripts/wordsService'
 
 @Component
-export default class CandidateWords extends Vue
-{
-    word: string
-    candidatesList: string[]
+export default class CandidateWords extends Vue {
+  word: string
+  candidatesList: string[]
 
-    constructor()
-    {
-        super()
-        this.word = ''
-        this.candidatesList = []
-    }
+  constructor() {
+    super()
+    this.word = ''
+    this.candidatesList = []
+  }
 
-    update(current: string)
-    {
-        this.candidatesList = WordsService.getCandWords(current);
-        for(const str of this.candidatesList)
-        {
-            console.log(str);
-        }
+  log() {
+    console.log(this.update('ac'))
+  }
+
+  data() {
+    return {
+      text: '',
     }
+  }
+
+  update(current: string) {
+    this.candidatesList = WordsService.getCandWords(current)
+    for (const str of this.candidatesList) {
+      console.log(str)
+    }
+  }
 }
 </script>
