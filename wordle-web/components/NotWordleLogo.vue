0ff7af9acf137de3481493c3f3fca0ea897ab680
Harjasleen Gill <hgill3@eagles.ewu.edu>
2022-05-10 17:10:56 -0700

    Added the logo to index page

--- wordle-web/components/NotWordleLogo.vue
+++ wordle-web/components/NotWordleLogo.vue
@@ -1,11 +1,9 @@
 <template>
-  <v-icon class="notwordle-logo" size="180">mdi-file-word-box</v-icon>
+  <v-img class="notwordle-logo" src="logo.jpeg" />
 </template>
 
 <style>
 .notwordle-logo {
-  max-height: 180px;
-  max-width: 180px;
   transform: rotateY(720deg);
   animation: turn 2.5s ease-out forwards 0.5s;
 }
