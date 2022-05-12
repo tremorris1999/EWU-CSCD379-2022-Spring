18e846aae95f51ef9afecd7265acb0476187ea5f
linhtran8699 <103064665+linhtran8699@users.noreply.github.com>
2022-05-11 21:39:12 -0700

    add animation to leaderboard

--- wordle-web/components/LeaderBoard2.vue
+++ wordle-web/components/LeaderBoard2.vue
@@ -7,35 +7,105 @@
     </v-container>
 
     <v-container>
-        <v-row>
-        <v-col col = "3" justify="center"> Name </v-col>
-        <v-col col = "3" justify="center"> Game Count </v-col>
+    <v-row justify="center">
+      <v-col cols="8">
+        <v-card color="transparent" flat>
+          <v-card-title class="justify-center">
+            <starLogo /> <starLogo /> <starLogo />
+          </v-card-title>
+          <v-card color="transparent" flat>
+            <v-card-title class="justify-center text-h4 font-weight-bold">
+              Top Awesome Players
+            </v-card-title>
+            </v-card-actions>
+          </v-card>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+
+    <v-container>
+      <v-card>
+        <v-row class="grow" color = "green" ></v-row>
+        </v-card>
+
+
+        <!-- <v-col col = "3" justify="center"> Game Count </v-col>
         <v-col col = "3" justify="center"> Average Guesses </v-col>
-        <v-col col = "3" justify="center"> Average Seconds/Game </v-col>
-      </v-row>    
+        <v-col col = "3" justify="center"> Average Seconds/Game </v-col> -->
+
       
     </v-container>
 
-    <v-container v-if="hasData">
+    <v-container px-5>
+    <v-row justify="center" >
+      <v-col cols="12">
+        <v-card color="green">
+          <v-card-title>Chocolate cheesecake recipe</v-card-title>
+          <c-card-text>
+            <ul>
+              <li>5 eggs</li>
+              <li>1 cup of warm milk</li>
+
+
+  <v-container v-if="hasData">
     <v-row 
       v-for="t in arr"
       :key="t"
       justify="center"
+      color = "white"
+      rounded
       >
-      <v-col col="3"  justify="center">
+      <v-col col="5"  justify="center">
         {{t.name}}
       </v-col>
-      <v-col cols="3">
+
+      <v-col cols="2">
         {{t.gameCount}}
       </v-col>
-      <v-col cols="3">
+
+      <v-col cols="2">
         {{t.averageGuesses}}
       </v-col>
       <v-col cols="3">
         {{t.averageSecondsPerGame}}
       </v-col>
     </v-row>
+
     </v-container>
+
+              <li>...</li>
+            </ul>
+          </c-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+
+
+
+    <!-- <v-container v-if="hasData">
+    <v-row 
+      v-for="t in arr"
+      :key="t"
+      justify="center"
+      >
+      <v-col col="5"  justify="center">
+        {{t.name}}
+      </v-col>
+
+      <v-col cols="2">
+        {{t.gameCount}}
+      </v-col>
+
+      <v-col cols="2">
+        {{t.averageGuesses}}
+      </v-col>
+      <v-col cols="3">
+        {{t.averageSecondsPerGame}}
+      </v-col>
+    </v-row>
+    </v-container> -->
 
   </v-container>
 </template>
