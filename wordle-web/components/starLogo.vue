7bc44b998a4bc1e3dcc58783bb2795aa96c69a29
linhtran8699 <103064665+linhtran8699@users.noreply.github.com>
2022-05-11 23:45:03 -0700

    logo change

--- wordle-web/components/starLogo.vue
+++ wordle-web/components/starLogo.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-icon class="star-logo" color="yellow" size="150">mdi-star</v-icon>
+  <v-icon class="star-logo" color="yellow" size="50">mdi-star</v-icon>
 </template>
 
 <style>
 .star-logo {
-  max-height: 180px;
+  max-height: 100px;
   max-width: 200px;
   transform: rotateY(1400deg);
   animation: turn 2.5s ease-out forwards 0.5s;
