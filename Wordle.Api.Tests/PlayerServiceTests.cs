e39790c4ab80aa0963b5c6a68154a05d66f3ba0a
schuyler <sasplin@ewu.edu>
2022-05-12 22:33:09 -0700

    ReturnsList Test

--- Wordle.Api.Tests/PlayerServiceTests.cs
+++ Wordle.Api.Tests/PlayerServiceTests.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Data.Sqlite;
 using Wordle.Api.Data;
 using Wordle.Api.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Wordle.Api.Tests;
 
@@ -31,6 +34,44 @@ public class PlayerServiceTests
     [TestMethod]
     public void Get_ReturnsList_Success()
     {
+        PlayerService subject = new(context!);
+        
+        
+        List<Player> dummyPlayers = new();
+
+        Player SmartyPants = new Player{
+            Name = "schuyler", 
+            GameCount = 5, 
+            AverageGuesses = 2.5, 
+            AverageSecondsPerGame = 30};
+        
+        Player Linh = new Player{
+                Name = "Linh", 
+                GameCount = 6, 
+                AverageGuesses = 2, 
+                AverageSecondsPerGame = 20};
+
+        Player Trover = new Player{
+            Name = "Trover", 
+            GameCount = 35, 
+            AverageGuesses = 5.8, 
+            AverageSecondsPerGame = 500};
+            
+        dummyPlayers.Append(Linh);
+        dummyPlayers.Append(SmartyPants);
+        dummyPlayers.Append(Trover);
+
+        subject.Update(SmartyPants.Name, (int)SmartyPants.AverageGuesses, SmartyPants.AverageSecondsPerGame);
+        subject.Update(Trover.Name, (int)Trover.AverageGuesses, Trover.AverageSecondsPerGame);
+        subject.Update(Linh.Name, (int)Linh.AverageGuesses, Linh.AverageSecondsPerGame);
+        
+        IEnumerable<Player> playersLocal = subject.GetPlayers(); 
+        
+
+        for (int i = 0; i < playersLocal.Count()-1; i++) {
+            Assert.IsTrue(playersLocal.ElementAt(i).AverageGuesses < playersLocal.ElementAt(i+1).AverageGuesses);
+        }
+        
 
     }
 
