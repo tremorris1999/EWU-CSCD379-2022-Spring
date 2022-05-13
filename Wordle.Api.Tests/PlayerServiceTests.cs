4949c6e1f93c5638ef5736a7b98243eda53e002c
Trevor Morris <tremorris1999@gmail.com>
2022-05-12 21:43:27 -0700

    tested constructor

--- Wordle.Api.Tests/PlayerServiceTests.cs
+++ Wordle.Api.Tests/PlayerServiceTests.cs
@@ -1,15 +1,31 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
 using Wordle.Api.Data;
+using Wordle.Api.Services;
 
 namespace Wordle.Api.Tests;
 
 [TestClass]
 public class PlayerServiceTests
 {
+    private AppDbContext? context;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        var contextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlite(new SqliteConnection("DataSource=:memory:"))
+            .Options;
+        context = new AppDbContext(contextOptions);
+        context.Database.Migrate();
+    }
+
     [TestMethod]
     public void Constructor_Success()
     {
-        
+        PlayerService subject = new(context!);
+        Assert.IsNotNull(subject);
     }
 
     [TestMethod]
