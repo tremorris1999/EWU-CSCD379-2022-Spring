3cc10485a33d8723a897f36fafccbab52f1a6c47
Grant Erickson <grant@granterickson.com>
2022-05-17 15:22:27 -0700

    Implement GetDailyWord based on the date

--- Wordle.Api/Controllers/DateWordController.cs
+++ Wordle.Api/Controllers/DateWordController.cs
@@ -1,13 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Wordle.Api.Data;
 
 namespace Wordle.Api.Controllers;
 public class DateWordController : Controller
 {
-    [HttpGet]
-    public string GetDailyWord(DateTime date)
+    private readonly AppDbContext _context;
+
+    public DateWordController(AppDbContext context)
     {
-        
-        return "Zebra";
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<string?> GetDailyWord(DateTime date)
+    {
+        //Sanitize the date by dropping time data
+        date = date.Date;
+        if (date.ToUniversalTime() >= System.DateTime.Today.ToUniversalTime().AddDays(0.5))
+        {
+            return null;
+        }
+        //Check if the day has a word in the database
+        DateWord? wordOfTheDay = _context.DateWords
+            .Include(x => x.Word)
+            .FirstOrDefault(dw => dw.Date == date);
+
+        //Mutex magic
+
+        if (wordOfTheDay != null)
+        //Yes: return the word
+        {
+            return wordOfTheDay.Word.Value;
+        }
+        else
+        {
+            //No: get a random word from our list
+            int wordCount = _context.Words.Count();
+            int randomIndex = new Random().Next(0, wordCount);
+            Word chosenWord = _context.Words
+                .OrderBy(w => w.WordId)
+                .Skip(randomIndex)
+                .Take(1)
+                .First();
+            //Save the word to the database with the date
+            _context.DateWords.Add(new DateWord { Date = date, Word = chosenWord });
+            await _context.SaveChangesAsync();
+            //Return the word
+            return chosenWord.Value;
+        }
     }
 }
 
