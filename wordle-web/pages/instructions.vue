45bf7f1fff603c3bbcc1f08314f9f6422e487372
linhtran8699 <103064665+linhtran8699@users.noreply.github.com>
2022-05-18 00:19:55 -0700

    more details to instructions page

--- wordle-web/pages/instructions.vue
+++ wordle-web/pages/instructions.vue
@@ -18,32 +18,49 @@
             <v-container >
               <v-card>
                 <v-card-text>
-                    <v-icon>mdi-check</v-icon>
-                    You have to guess the !Wordle in six guesses or less. <v-spacer></v-spacer>
-                     <v-icon>mdi-check</v-icon> Letters can be used more than once. <v-spacer></v-spacer>
-                     <v-icon>mdi-check</v-icon> Answers are never plurals.<v-spacer></v-spacer>   
-                     <h-2>Examples</h-2> <v-spacer></v-spacer>                                 
+                    <v-icon class="pa-0 my-2">mdi-check</v-icon>You have to guess the !Wordle in six guesses or less. <v-spacer></v-spacer>
+                     <v-icon class="pa-0 my-2">mdi-check</v-icon> Letters can be used more than once. <v-spacer></v-spacer>
+                     <v-icon class="pa-0 my-2">mdi-check</v-icon> Answers are never plurals.<v-spacer></v-spacer>   
+                     <h-2>Example:</h-2> <v-spacer></v-spacer>                                 
                     
                     <!-- BASIC word -->
-                    <v-btn color=success small class="pa-0 my-4">B</v-btn> 
-                    <v-btn color=error small class="pa-0 my-4">A</v-btn> 
+                    <v-btn color=error small class="pa-0 my-4">B</v-btn> 
+                    <v-btn color=success small class="pa-0 my-4">A</v-btn> 
                     <v-btn color=error small class="pa-0 my-4">S</v-btn> 
                     <v-btn color=error small class="pa-0 my-4">I</v-btn> 
                     <v-btn color=error small class="pa-0 my-4">C</v-btn>
                     <v-spacer></v-spacer>
 
-                    The B is IN the word and in the CORRECT spot, the rest are not in the word <v-spacer></v-spacer>
+                    The A is IN the word but in the WRONG spot, the rest are not in the word. <v-spacer></v-spacer>
                     
 
                     <!-- APPLE word -->
-                    <v-btn color=error small class="pa-0 my-4">A</v-btn> 
+                    <v-btn color=success small class="pa-0 my-4">A</v-btn> 
                     <v-btn color=error small class="pa-0 my-4">P</v-btn> 
                     <v-btn color=error small class="pa-0 my-4">P</v-btn> 
                     <v-btn color=warning small class="pa-0 my-4">L</v-btn> 
-                    <v-btn color=error small class="pa-0 my-4">E</v-btn>
+                    <v-btn color=warning small class="pa-0 my-4">E</v-btn>
                     <v-spacer></v-spacer>
-                    The L is IN the word but in the WRONG spot, the rest are not in the word
-                    <v-spacer></v-spacer>      
+                    The A is IN the word and in the CORRECT spot.<v-spacer></v-spacer>
+                    The L, E is IN the word but in the WRONG spot, the rest are not in the word.
+                    <v-spacer></v-spacer>
+
+                    <!-- ANGEL word -->
+                    Correct word: 
+                    <v-btn color=success small class="pa-0 my-4">A</v-btn> 
+                    <v-btn color=success small class="pa-0 my-4">N</v-btn> 
+                    <v-btn color=success small class="pa-0 my-4">G</v-btn> 
+                    <v-btn color=success small class="pa-0 my-4">E</v-btn> 
+                    <v-btn color=success small class="pa-0 my-4">L</v-btn> 
+                    <v-spacer></v-spacer>                    
+
+                    <v-icon class="pa-0 my-2">mdi-lightbulb-on-outline</v-icon>
+                    There are a list of "available words" for you to choose from <v-spacer></v-spacer>  
+
+                    <v-icon class="pa-0 my-2">mdi-lightbulb-on-outline</v-icon>
+                    You can place <v-btn color=primary small class="pa-0 my-4">?</v-btn> anywhere and choose suggestion from the word list
+
+                    
                 
 
                 </v-card-text>
