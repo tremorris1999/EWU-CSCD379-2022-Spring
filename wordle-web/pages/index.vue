fb39393c2de1b376d8896cc95c16e6830f2f7bef
linhtran8699 <103064665+linhtran8699@users.noreply.github.com>
2022-05-31 23:24:27 -0700

    home page updated

--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -19,6 +19,15 @@
         </v-card>
       </v-col>
     </v-row>
+
+    <v-row>
+      <v-col align="center">
+        <v-btn nuxt to="/instructions" color="primary" x-large width="180px">
+          How to Play
+        </v-btn>
+      </v-col>
+    </v-row>
+
   </v-container>
 </template>
 
