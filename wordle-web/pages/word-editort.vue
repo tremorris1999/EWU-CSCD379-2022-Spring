3ab4b6a4db6cc594115af7baceec7d9fdba87d8a
Trevor Morris <tremorris1999@gmail.com>
2022-06-13 02:57:51 -0700

    adds pagination and fixes search

--- wordle-web/pages/word-editort.vue
+++ wordle-web/pages/word-editort.vue
@@ -11,29 +11,32 @@
             </v-col>
         </v-row>
     <v-row
-    v-for="w in this.results" :key="w"
+    v-for="w in this.pageContent" :key="w"
     >
     <v-col cols="1" />
     <v-col cols="3">
-    {{ w }}
+    {{ w.toUpperCase() }}
     </v-col>
     
     <v-col cols="3">
-        <v-btn>
+        <v-btn @click="editWord(w, true)">
             Make Common
         </v-btn>
     </v-col>
     <v-col cols="3">
-        <v-btn>
+        <v-btn @click="editWord(w, false)">
             Make Uncommon
         </v-btn>
     </v-col>
     <v-col cols="2">
-        <v-btn>
+        <v-btn color="info" @click="deleteWord(w)">
             Delete
         </v-btn>
     </v-col>
     </v-row>
+    <v-card-actions>
+        <v-pagination v-model="currentPage" :length="numPages" @input="goToPage"/>
+    </v-card-actions>
     </v-card>
 </v-container>
 </template>
@@ -48,33 +51,77 @@ export default class WordEditor extends Vue
     currentPage: number = 0
     wordlist: string[] = []
     results: string[] = []
+    pageContent: string[] = []
     query: string = ""
+    loading: boolean = true;
+    numPages: number = 0
 
     created(){
         this.$watch('query', (newQuery) => {
-            this.results = []
-            for(let w in this.wordlist)
-            {
-                if(w.match(newQuery))
-                    this.results.push(w)
-            }
+            this.results = this.wordlist.filter(item => item.startsWith(newQuery))
+            this.numPages = Math.round(this.results.length / this.pageSize)
+            this.goToPage(1)
         })
     }
 
     mounted()
     {
         
-        this.$axios.get("/api/Word/Get")
+        this.$axios
+            .get("/api/Word/Get")
             .then((response) => {
                 this.wordlist = response.data
-                this.goToPage(0)
+                this.results = this.wordlist
+                this.numPages = Math.round(this.results.length / this.pageSize)
+                this.goToPage(1)
+                this.loading = false;
             })
     }
 
     goToPage(index: number)
     {
+        console.log("\r\n Page " + index)
         this.currentPage = index
-        this.results = this.wordlist.slice(this.currentPage * this.pageSize, this.pageSize)
+        this.pageContent = this.results.slice((index - 1) * this.pageSize, index * this.pageSize)
+    }
+
+    addWord(value: string, common: boolean)
+    {
+        this.$axios
+            .post('/api/Word/Add', {
+            value: value,
+            common: common
+            })
+            .then((response) => {
+                // dialog maybe?
+                this.query = ""
+            })
+    }
+
+    editWord(value: string, common: boolean)
+    {
+        this.$axios
+            .put('/api/Word/ChangeCommon', {
+                value: value,
+                common: common
+            })
+            .then((response) => {
+                // dialog maybe?
+                this.query = ""
+            })
+    }
+
+    deleteWord(value: string)
+    {
+        this.$axios
+            .put('/api/Word/Delete', {
+                value: value,
+                common: true
+            })
+            .then((response) => {
+                // dialog maybe?
+                this.query = ""
+            })
     }
 
     
